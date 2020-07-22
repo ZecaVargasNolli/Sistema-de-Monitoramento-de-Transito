afcaa72fbef3667af631c31e93247f8a558b37b5
Giancarlo <Giancarlo@192.168.1.7>
2020-07-21 23:46:37 -0300

    Definição de algumas classes bases

--- ControladorTransito/src/controladortransito/Model/Malha.java
+++ ControladorTransito/src/controladortransito/Model/Malha.java
@@ -2,9 +2,25 @@
 package controladortransito.Model;
 
 /**
- *
+ * Classe que representa a malha viaria
+ * 
  * @author Giancarlo Pandini
  */
 public class Malha {
+    
+    private Nodo[][] nodos;
+
+    public Malha(int altura, int largura) {
+        this.nodos = new Nodo[altura][largura];
+    }
+    
+    public Nodo getNode(int altura, int largura) {
+        return this.nodos[altura][largura];
+    }
+    
+    public void setNode(int altura, int largura, Nodo nodo) {
+        this.nodos[altura][largura] = nodo;
+    }
+    
     
 }
--- ControladorTransito/src/controladortransito/Model/Nodo.java
+++ ControladorTransito/src/controladortransito/Model/Nodo.java
@@ -2,9 +2,37 @@
 package controladortransito.Model;
 
 /**
- *
+ * Classe que representa um nodo da malha viaria
+ * 
  * @author Giancarlo Pandini
  */
 public class Nodo {
     
+    private Carro carro;
+    private int direcao;
+
+    public Nodo(int direcao) {
+        this.direcao = direcao;
+    }
+    
+    public Carro getCarro() {
+        return carro;
+    }
+
+    public void setCarro(Carro carro) {
+        this.carro = carro;
+    }
+
+    public int getDirecao() {
+        return direcao;
+    }
+
+    public void setDirecao(int direcao) {
+        this.direcao = direcao;
+    }
+    
+    public boolean isCruzamento() {
+        /** @TODO desenvolver */
+        return false;
+    }
 }
